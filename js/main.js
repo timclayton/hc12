dfd489325af190b67fd20b3981b406b2560d3e92
matt.diehl <matt.diehl@berndtgroup.net>
2012-11-19 14:23:57 -0500

    Most recent version

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+window.onload = function() {
 
 	function getsupportedprop(proparray){
 		var root=document.documentElement //reference root element of document
@@ -18,13 +18,13 @@ $(document).ready(function() {
 					stuck,
 					csstransform 		=  getsupportedprop(['transform', 'MozTransform', 'WebkitTransform', 'msTransform', 'OTransform']);
 	
-	$(window).resize($.throttle(function() {
+	window.onresize = function() {
 		
 					wh						= window.innerHeight;
 		
-	}, 50));
+	}
 	
-	$(window).scroll($.throttle(function() {
+	window.onscroll = function() {
 					s 							= 	window.pageYOffset;  //  get scrollTop value
 					gPercent				=  s / (d-wh),	// global percent value
 					pos 						=	Math.floor( sPercent ),  //  calculated position
@@ -37,8 +37,10 @@ $(document).ready(function() {
 		scenes.forEach(setPos, this);
 		transforms[0].forEach(transform, this);
 		transforms[pos].forEach(transform, this);
+		classShifts[0].forEach(shiftClass, this);
+		classShifts[pos].forEach(shiftClass, this);
 		
-	}, 50));
+	}
 	
 	var stickScene = function() {  //  Adds proper class to current/prev scenes
 
@@ -55,12 +57,21 @@ $(document).ready(function() {
 		
 	}
 	
+	var duplicate = function(element) {
+		if ( element.repeat > 1 ) {  //  If the element loops, duplicate the order 
+			var order = element.order
+			for ( var i = 1; i < element.repeat; i++ ) {
+				var newOrder = element.order.concat(order);
+				element.order = newOrder;
+			}
+		}
+		element.total = element.order.length;
+	}
+	
 	var calcPercent = function(a,b,c) {	
 		
-		if ( a < b ) { var d = 0; }
-		else if ( a >= c ) { var d = 1; }
-		else { var d =  ( a - b ) / ( c - b ); }
-		return d;
+		if ( a <= b ) { return 0 }
+		else { return a >= c ? 1 : ( a - b ) / ( c - b ) }
 	
 	}
 	
@@ -99,29 +110,54 @@ $(document).ready(function() {
 			element.id.style.opacity=x
 			
 		}
+		
+		else if ( element.type === "bgShiftX" ) {
+			
+			element.id.style.backgroundPosition=x + "% 100%"
+			
+		}
 			
 	}
 	
+	var shiftClass = function(element, index, array) {
 	
+		if ( array === classShifts[0] ) { var animPercent = calcPercent(gPercent,element.start,element.end); }  // global transforms 
+		else { var animPercent = calcPercent(sPercent,element.start,element.end); }  //  scene specific transforms
+		
+		var orderPos = element.order[Math.floor( element.total * animPercent )];
+		if ( orderPos !== undefined ) { element.id.className = element.prefix + orderPos; }
+	
+	}
+	
+	var scrollTo = function(element, to, duration) {
+        if (duration < 0) return;
+        var difference = to - element.scrollTop;
+        var perTick = difference / duration * 10;
+        
+        setTimeout(function() {
+            element.scrollTop = element.scrollTop + perTick;
+            scrollTo(element, to, duration - 10);
+        }, 10);
+    }
 	
 	var scenes = [
 	
-		{scene:1, start:0, end:4000},
-		{scene:2, start:4000, end:12000},
-		{scene:3, start:12000, end:16000},
-		{scene:4, start:16000, end:22000}
+		{scene:1, start:0, end:3000},
+		{scene:2, start:3000, end:9000},
+		{scene:3, start:9000, end:17000},
+		{scene:4, start:17000, end:23000}
 	
 	]
 	
 	var transforms = [	// First array holds global transforms, following are per scene
 	
-		[
+		[  // global
 		
-			{id:document.getElementById("candycane1"), start: 0.07, end: 0.3, x: 60, y: -400, type:"translate"},
+			{id:document.getElementById("candycane1"), start: 0.07, end: 0.15, x: 60, y: -400, type:"translate"},
 		
 		],
 		
-		[
+		[  // scene 1
 			
 			{id:document.getElementById("strike-through"), start: 1.1, end: 1.2, x: 100, y: 50, type:"size"},
 			{id:document.getElementById("text-john-berndts"), start: 1.25, end: 1.4, x: 1, y: 0, type:"opacity"},
@@ -133,26 +169,56 @@ $(document).ready(function() {
 						
 		],
 		
-		[
+		[  // scene 2
+
+			{id:document.getElementById("textbox1"), start: 2.05, end: 2.15, x: 1, y: 0, type:"opacity"},
+			{id:document.getElementById("textbox2"), start: 2.3, end: 2.4, x: 1, y: 0, type:"opacity"}
 		
-			{id:document.getElementById("textbox1"), start: 2.05, end: 2.2, x: 1, y: 0, type:"opacity"},
-			{id:document.getElementById("textbox2"), start: 2.4, end: 2.55, x: 1, y: 0, type:"opacity"}
+		],
+		
+		[  // scene 3
+		
+			{id:document.getElementById("ericw"), start: 3, end: 3.05, x: 1, y: 0, type:"opacity"},
+			{id:document.getElementById("ericw"), start: 3.4, end: 3.6, x: -800, y: 0, type:"translate"},
+			{id:document.getElementById("s3bg"), start: 3.4, end: 3.6, x: 100, y: 0, type:"bgShiftX"}
 		
 		]
 		
 	
 	]
 	
+	var classShifts = [  // First array holds global class shifts, following are per scene
 	
+		[  // global
+		
+		],
+		
+		[  // scene 1
+		
+		],
+	
+		[  // scene 2
+		
+		],
+		
+		[  // scene 3
+		
+			{id:document.getElementById("ericw"), start: 3.1, end: 3.7, prefix: "ericw", order: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17], repeat:2}
+		
+		]
+	
+	]
+	
+	for ( i = 0; i < classShifts.length; i++ ) {
+		classShifts[i].forEach(duplicate, this);
+	}
 	
 	window.scrollTo(0,0);
 	
-	$("#play").click(function() {  //  animate scroll
-		$("html,body").animate({
-			scrollTop: d-wh
-		}, 20000, 'linear');
-	});
+	document.getElementById("play").onclick = function () {
+	   scrollTo(document.body, d-wh-2, 20000);   
+	}
 	
 	stickScene();  //  Apply fixed positioning to first scene
 	
-});
+}
