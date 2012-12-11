850af2942862bac31873de594199cf26a3ec7904
Tim Clayton <tjclayton75@gmail.com>
2012-12-11 13:02:28 -0500

    in the middle of adjusting scene length

--- js/main.js
+++ js/main.js
@@ -478,7 +478,7 @@ window.onload = function() {
 			{id:document.getElementById("back-snow"), start: 3.1, end: 3.99, x: 0, y: 150, type:"translate"},
 
 			{id:sections[2], start: 3.2, end: 3.35, x: 1, y: 0, type:"opacity", fade: "in"},
-			{id:sections[2], start: 3.95, end: 3.99, x: -1.1, y: 0, type:"opacity", fade: "out"},
+			{id:sections[2], start: 3.85, end: 3.99, x: -1.1, y: 0, type:"opacity", fade: "out"},
 
 			{id:narrationFiles[2], start: 3.01, end: 3.2, x: 1, y: 0, type:"narration"}, 
 			
